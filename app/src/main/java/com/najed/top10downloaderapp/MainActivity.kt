d73c81d32f98c61719b6e6646a0e49e74494c5eb
plateauh <70834431+plateauh@users.noreply.github.com>
2021-10-18 18:21:35 +0300

    initial

---
+++ app/src/main/java/com/najed/top10downloaderapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.najed.top10downloaderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
